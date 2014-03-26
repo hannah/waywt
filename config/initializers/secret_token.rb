fc2505c3b04d4d6601b41974fe97ce53255142d1
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-03-26 15:24:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Waywt::Application.config.secret_key_base = '294125d909ba28eb0f2957d216bdb380ab20d7ddd859a59ad7d7144198f0ecd2dad7faeedbace1776f424ddc0a5b176c083f6f4ff06f5b3d86a304923f063f73'

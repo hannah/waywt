56950afe8b883e2578d921d2c0799a0c12932dcd
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-04 09:35:18 -0400

    initial configuration and first acceptance test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Waywt::Application.routes.draw do
+  root 'index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

1a6f0a1c882494f4f25caa15482ce8c7441b5796
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-20 18:35:11 -0400

    created a new feature test for outfit

--- spec/features/upload_new_outfit_spec.rb
+++ spec/features/upload_new_outfit_spec.rb
@@ -15,6 +15,18 @@ User can submit and save a post
 
 =end
 
+  scenario 'submit a new outfit' do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+
+    visit new_outfit_path
+    outfit = FactoryGirl.build(:outfit)
+    fill_in 'Description', with: outfit.description
+    attach_file 'outfit_image', File.join(Rails.root, outfit.image)
+    click_on 'Submit Outfit'
+    expect(page).to have_content('Outfit Submitted')
+    expect(page).to have_content()
+  end
 
 
 end

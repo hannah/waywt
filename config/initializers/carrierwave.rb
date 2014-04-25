5e6da8a60838fe556bf5f9ed52a69163e5dfcb29
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-25 14:36:47 -0400

    fixed deployment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-    aws_secret_access_key: ENV['AWS_SECRET_KEY']
-  }
+if production? do
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY']
+    }
 
-  config.fog_directory = "waywt-#{Rails.env}"
+    config.fog_directory = "waywt-#{Rails.env}"
+  end
 end

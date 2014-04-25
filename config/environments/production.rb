8ec7033a0565c795a1b3e636a3149b9f68ff6cb1
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-25 14:39:56 -0400

    actually fixed carrierwave AWS this time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,4 +13,14 @@ Waywt::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
+
+  CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY']
+  }
+
+  config.fog_directory = "waywt-#{Rails.env}"
+  end
 end

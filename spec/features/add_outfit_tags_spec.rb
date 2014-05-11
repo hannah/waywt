cd80b4870c371fe99cc0c35753d7c049e899ee65
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-05-10 22:55:22 -0400

    WIP: add tags

--- spec/features/add_outfit_tags_spec.rb
+++ spec/features/add_outfit_tags_spec.rb
@@ -1,17 +1,21 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# feature 'user adds tags', %Q{
-#   As a user,
-#   I want to be able to tag my picture with brands
-#   In order to be able to include it in searches for the brand
-# } do
+feature 'user adds tags', %Q{
+  As a user,
+  I want to be able to tag my picture with brands
+  In order to be able to include it in searches for the brand
+} do
 
-#   scenario 'a user adds tags to an outfit' do
-#     user = FactoryGirl.create(:user)
-#     sign_in_as(user)
-#     outfit = FactoryGirl.create(:outfit, user: user)
-#     visit outfit_path(outfit)
-#     fill_in 'Tags' with 'Awesome'
-#     fill_in 'Tags' with 'Very Cool'
-#   end
-# end
+  scenario 'a user adds tags to an outfit' do
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+    outfit = FactoryGirl.create(:outfit, user: user)
+    visit outfit_path(outfit)
+    fill_in 'Tags', with: 'Awesome'
+    fill_in 'Tags', with: 'Very Cool'
+    click_on 'Add tags'
+    expect(page).to have_content 'Tags added!'
+    expect(page).to have_content 'Awesome'
+    expect(page).to have_content 'Very Cool'
+  end
+end

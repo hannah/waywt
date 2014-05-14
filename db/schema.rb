a92c7484264c4d812956fb5d1da13cab8f53e8c8
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-05-13 23:06:19 -0400

    WIP: adding tags model to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421155427) do
+ActiveRecord::Schema.define(version: 20140514000825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20140421155427) do
     t.string  "image",       null: false
     t.text    "description"
     t.integer "user_id",     null: false
+  end
+
+  create_table "tags", force: true do |t|
+    t.string "tag"
   end
 
   create_table "users", force: true do |t|
